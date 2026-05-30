537b9e73b247cdb75ed4488bb83d3d127637cc59
Amiruddin Irsyad <amier.dev@gmail.com>
2026-05-30 13:40:16 +0700

    router for transaction

--- src/router.js
+++ src/router.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const materialRouter =
-    require('./api/master/material/material.controller')
-const roleRouter =
-    require('./api/master/role/role.controller')    
-// const carModelRouter = 
-//     require('./api/master/carModel/carModel')
-const errorTestRouter =
-    require('./api/master/error-test/test') 
-//auth
+
+const materialRouter = require('./api/master/material/material.controller');
+const roleRouter = require('./api/master/role/role.controller');
+const carModelRouter = require('./api/master/carModel/carModel.controller');
+const customerRouter = require('./api/master/customer/customer.controller');
+const logisticRouter = require('./api/master/logistic/logistic.controller');
+const supplierRouter = require('./api/master/supplier/supplier.controller');
+const bomRouter = require('./api/master/bom/bom.controller');
+const rolePermissionsRouter = require('./api/master/rolePermissions/rolePermissions.controller');
+const userRouter = require('./api/master/user/user.controller');
+
+const errorTestRouter = require('./api/master/error-test/test');
 const authRouter = require('./api/auth/auth.controller');
+const productionTransactionRouter = require('./api/transaction/production/production.controller');
 
+router.use('/material', materialRouter);
+router.use('/role', roleRouter);
+router.use('/car-model', carModelRouter);
+router.use('/customer', customerRouter);
+router.use('/logistic', logisticRouter);
+router.use('/supplier', supplierRouter);
+router.use('/bom', bomRouter);
+router.use('/role-permissions', rolePermissionsRouter);
+router.use('/user', userRouter);
 
-router.use('/material', materialRouter)
-router.use('/role', roleRouter)
-// router.use('/car-model', carModelRouter)
-router.use('/error-test', errorTestRouter)
-//auth
-router.use('/auth', authRouter)
+// Transaction Routes
+router.use('/transaction/production', productionTransactionRouter);
 
-//default export
+router.use('/error-test', errorTestRouter);
+router.use('/auth', authRouter);
+
 module.exports = router;
