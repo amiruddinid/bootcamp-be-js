aa70a90df722fce173db32e5dd79573b84d5bf40
Amiruddin Irsyad <amier.dev@gmail.com>
2026-06-03 11:33:26 +0700

    cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express'); // import library express untuk buat server
 const router = require('./src/router'); // import router yang sudah dibuat
 const app = express(); // buat objek express yang akan menampung route dan middleware
+const cors = require('cors'); // import library cors untuk mengizinkan akses dari domain lain
 const port = 1337; // port tempat server akan dijalankan
 const { autoOrderJob, autoOrderJob2 } = 
     require('./src/jobs/orderMaterial/autoOrderJob'); // import job untuk auto order material
 
+app.use(cors());
 app.use(express.json())
 
 app.use('/api', router);
