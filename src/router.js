fd85a803648c33623f9f90ccb4543ef24b31716f
Amiruddin Irsyad <amier.dev@gmail.com>
2026-06-11 09:20:56 +0700

    router enable

--- src/router.js
+++ src/router.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const materialRouter = require('./api/master/material/material.controller');
 const roleRouter = require('./api/master/role/role.controller');
-// const carModelRouter = require('./api/master/carModel/carModel.controller');
-// const customerRouter = require('./api/master/customer/customer.controller');
-// const logisticRouter = require('./api/master/logistic/logistic.controller');
-// const supplierRouter = require('./api/master/supplier/supplier.controller');
-// const bomRouter = require('./api/master/bom/bom.controller');
-// const rolePermissionsRouter = require('./api/master/rolePermissions/rolePermissions.controller');
-// const userRouter = require('./api/master/user/user.controller');
+const carModelRouter = require('./api/master/carModel/carModel.controller');
+const customerRouter = require('./api/master/customer/customer.controller');
+const logisticRouter = require('./api/master/logistic/logistic.controller');
+const supplierRouter = require('./api/master/supplier/supplier.controller');
+const bomRouter = require('./api/master/bom/bom.controller');
+const rolePermissionsRouter = require('./api/master/rolePermissions/rolePermissions.controller');
+const userRouter = require('./api/master/user/user.controller');
 
 const errorTestRouter = require('./api/master/error-test/test');
 const authRouter = require('./api/auth/auth.controller');
@@ -17,13 +17,13 @@ const productionTransactionRouter = require('./api/transaction/production/produc
 
 router.use('/material', materialRouter);
 router.use('/role', roleRouter);
-// router.use('/car-model', carModelRouter);
-// router.use('/customer', customerRouter);
-// router.use('/logistic', logisticRouter);
-// router.use('/supplier', supplierRouter);
-// router.use('/bom', bomRouter);
-// router.use('/role-permissions', rolePermissionsRouter);
-// router.use('/user', userRouter);
+router.use('/car-model', carModelRouter);
+router.use('/customer', customerRouter);
+router.use('/logistic', logisticRouter);
+router.use('/supplier', supplierRouter);
+router.use('/bom', bomRouter);
+router.use('/role-permissions', rolePermissionsRouter);
+router.use('/user', userRouter);
 
 // Transaction Routes
 router.use('/transaction/production', productionTransactionRouter);
